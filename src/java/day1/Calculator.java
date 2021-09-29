6306c6e764249c2f96d8675b3369d22f25f20c78
ARUNEISEN <aruneisen@gmail.com>
2021-09-29 12:21:03 +0530

    my first commit

---
+++ src/java/day1/Calculator.java
@@ -0,0 +1,12 @@
+package java.day1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+	public void add() {
+		System.out.println("addition");
+	}
+
+}
